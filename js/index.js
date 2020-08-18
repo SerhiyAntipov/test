b0d262c86b387929cfd3aa16cda77b149fb056ee
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-08-18 22:26:16 +0300

    add active menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 
 window.onload = function () {
+	// menu active
+	let mainNavLink = document.querySelectorAll('.main-nav__link');
+	let mainNavLinkActive = 0;
+
+	mainNavLink.forEach(function (obj, i) {
+		mainNavLink[i].addEventListener('click', function () {
+			if (mainNavLink[i].classList.contains('main-nav__link-active') != true) {
+				mainNavLink[i].classList.add('main-nav__link-active');
+				mainNavLink[mainNavLinkActive].classList.toggle('main-nav__link-active');
+				mainNavLinkActive = mainNavLink[i].getAttribute('data-main-nav-id')
+			}
+		});
+	});
+
+
+	// Menu closing when clicking on the link
 	const mainNav = document.querySelector('.main-nav__ul');
 	mainNav.addEventListener('click', function (event) {
 		if (event.srcElement.tagName == "A" || event.srcElement.localName == "a") {
@@ -7,7 +23,7 @@ window.onload = function () {
 		}
 	});
 
-
+	// Smooth scroll 
 	let scroll = new SmoothScroll('a[href*="#"]', {
 		speed: 500,
 		speedAsDuration: true
