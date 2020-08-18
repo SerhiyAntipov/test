eea4c3ced5df2ac3779bd34dc1bf5111938b1699
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-08-18 21:53:24 +0300

    add smooth scroll

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-let scroll = new SmoothScroll('a[href*="#"]', {
-	speed: 500,
-	speedAsDuration: true
-});
+
+window.onload = function () {
+	const mainNav = document.querySelector('.main-nav__ul');
+	mainNav.addEventListener('click', function (event) {
+		if (event.srcElement.tagName == "A" || event.srcElement.localName == "a") {
+			document.querySelector('#main-nav-call').checked = false;
+		}
+	});
+
+
+	let scroll = new SmoothScroll('a[href*="#"]', {
+		speed: 500,
+		speedAsDuration: true
+	});
+}
+
+
